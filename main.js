36f0d4bcaae9f99ba258d3d58fe1f54d53db356a
mdmahidmahmud <69872745+mdmahidmahmud@users.noreply.github.com>
2020-08-19 02:42:34 +0600

    owl-carousel js

---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$('.container').owlCarousel({
+		loop:true,
+		margin:10,
+		nav:true,
+		dots:true,
+		autoplay:true,
+		autoplayTimeout:5000,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:1
+			},
+			1000:{
+				items:1
+			}
+		}
+	})
+});
